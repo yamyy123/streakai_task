28e46f6314fbfb0b807f16ed0aa8e387b083db52
MOHAMMED YAMEEN S M <mohammedyameen0603@gmail.com>
2025-02-04 21:46:47 +0530

    new changes

--- handler/handler.go
+++ handler/handler.go
@@ -10,16 +10,17 @@ import (
 func DfsHandler(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPost {
 		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
 	}
 	w.Header().Set("Content-Type", "application/json")
 
-	var request *models.DfsRequest
+	var request models.DfsRequest
 	err := json.NewDecoder(r.Body).Decode(&request)
 	if err != nil {
 		http.Error(w, "Error parsing request body", http.StatusBadRequest)
 		return
 	}
-	res, errors := service.CalculateDfs(request)
+	res, errors := service.CalculateDfs(&request)
 	if errors != nil {
 		w.WriteHeader(http.StatusBadRequest)
 		json.NewEncoder(w).Encode(errors)
